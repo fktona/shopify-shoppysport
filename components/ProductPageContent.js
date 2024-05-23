f5c2262d3b7e9b2aa11403b5e9d7d5d8a41e9c2e
fktona <adetona.fk@gmail.com>
2024-05-23 05:12:49 -0700

    [completed]: finist the mxajor aspect and functionality of the app

--- components/ProductPageContent.js
+++ components/ProductPageContent.js
@@ -1,8 +1,8 @@
 'use client'
 import Image from 'next/image'
 import ProductForm from './ProductForm'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore, { Navigation, Pagination } from 'swiper'
+// import { Swiper, SwiperSlide } from 'swiper/react'
+//import SwiperCore, { Navigation, Pagination } from 'swiper'
 import RecommendedList from './RecommendedList'
 
 export default function ProductPageContent({ product }) {
@@ -11,20 +11,20 @@ export default function ProductPageContent({ product }) {
 
   product.images.edges.map((image, i) => {
     images.push(
-      <SwiperSlide key={`slide-${i}`}>
+      <div key={`slide-${i}`}>
         <Image src={image.node.url} alt={image.node.altText} layout="fill" objectFit="cover" />
-      </SwiperSlide>
+      </div>
     )
   })
 
-  SwiperCore.use([Navigation, Pagination])
+  // SwiperCore.use([Navigation, Pagination])
 
   return (
     <div>
       <div className="flex flex-col items-center justify-center w-11/12 max-w-6xl mx-auto space-y-8 md:flex-row md:items-start md:space-y-0 md:space-x-4 lg:space-x-8">
         <div className="w-full max-w-md overflow-hidden bg-white border shadow-lg rounded-2xl md:w-1/2">
           <div className="relative w-full h-96">
-            <Swiper
+            <div
               style={{ '--swiper-navigation-color': '#000', '--swiper-pagination-color': '#000' }}
               navigation
               pagination={{ clickable: true }}
@@ -32,7 +32,7 @@ export default function ProductPageContent({ product }) {
               loop="true"
             >
               {images}
-            </Swiper>
+            </div>
           </div>
         </div>
         <ProductForm product={product} />
